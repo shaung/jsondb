241f51ead4b0aa0565768255398a75e119d72dcf
shaung <shaun.geng@gmail.com>
2012-07-15 23:17:54 +0900

    Tests for travis

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -162,6 +162,14 @@ class TestBookStore:
     def teardown(self):
         self.db.close()
 
+    def test_internal_eq(self):
+        result = self.db.backend.select("select t.id from jsondata t where t.id in (4,9,14,20) and exists (select * from (select id, type, value from jsondata where parent = (select id from jsondata where type = 8 and parent = t.id and value = 'author') union all select -9, -1, NULL) __t0__ where (__t0__.type > 0 and __t0__.value = 'Evelyn Waugh') and (__t0__.type > 0))")
+        eq_([row['id'] for row in result], [9])
+
+    def test_internal_eq_2(self):
+        result = self.db.backend.select("select t.id from jsondata t where t.id in (4,9,14,20) and exists (select * from (select id, type, value from jsondata where parent = (select id from jsondata where type = 8 and parent = t.id and value = 'author') union all select -9, -1, NULL) __t0__ where (__t0__.type > 0 and __t0__.value = ?) and (__t0__.type > 0))", ('Evelyn Waugh',))
+        eq_([row['id'] for row in result], [9])
+ 
     def test_condition_eq(self):
         path = '$.store.book[?(@.author="Evelyn Waugh")].title'
         self.eq(path, ['Sword of Honour'])
