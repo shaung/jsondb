8ec0fa7eaadc5e01313dfb762c81a83b9228817e
shaung <shaun.geng@gmail.com>
2012-07-15 23:31:58 +0900

    Tests for sqlite

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -170,6 +170,10 @@ class TestBookStore:
         result = self.db.backend.select("select t.id from jsondata t where t.id in (4,9,14,20) and exists (select * from (select id, type, value from jsondata where parent = (select id from jsondata where type = 8 and parent = t.id and value = 'author') union all select -9, -1, NULL) __t0__ where (__t0__.type > 0 and __t0__.value = ?) and (__t0__.type > 0))", ('Evelyn Waugh',))
         eq_([row['id'] for row in result], [9])
  
+    def test_travis_eq(self):
+        result = self.db.backend.select("select id, value from jsondata where value = 'Evelyn Waugh'")
+        eq_([row['value'] for row in result], ['Evelyn Waugh'])
+
     def test_condition_eq(self):
         path = '$.store.book[?(@.author="Evelyn Waugh")].title'
         self.eq(path, ['Sword of Honour'])
